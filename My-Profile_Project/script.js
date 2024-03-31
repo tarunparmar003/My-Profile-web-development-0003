30dabcccb60bb6ab84b39649e54fafc7ea7413c2
TARUN PARMAR <126954777+tarunparmar003@users.noreply.github.com>
2024-03-31 14:40:27 +0530

    Add files via upload

---
+++ My-Profile_Project/script.js
@@ -0,0 +1,11 @@
+var opencontainer = function() {
+    window.location.href = "http://127.0.0.1:5500/JavaScript/js-Dom/My-Profile_Project/index2.html"; 
+}
+
+var backToMainPage = function() {
+    window.location.href = "http://127.0.0.1:5500/JavaScript/js-Dom/My-Profile_Project/index.html";
+}
+
+var openinstagram = function(){
+    window.location.href="https://www.instagram.com/_tarun___rajpoot_/";
+}
